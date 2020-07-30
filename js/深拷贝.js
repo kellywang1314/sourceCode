130d70bcb6ebe9c8887a094b8b37235e1f2fef09
王亚男 <wangyanan05@koolearn-inc.com>
2020-07-30 22:38:01 +0800

    feat: 增加深拷贝

---
+++ js/深拷贝.js
@@ -0,0 +1,39 @@
+// 浅拷贝
+function copy(obj){
+    let newObj
+    if(typeof obj === 'object'){
+        newObj = {}
+        for(let i in obj){
+            newObj[i] = obj[i]
+        }
+    }else{
+        newObj = obj
+    }
+    return newObj
+}
+
+
+// 深拷贝
+function deepCopy(obj,hash = new WeakMap()){
+    if(typeof obj !== 'object') return obj
+    if(hash.has(obj)) return hash.get(obj)
+    let newObj = Object.prototype.toString.call(obj) === '[object Object]' ? {} : []
+    hash.set(obj,newObj)
+    for(let i in obj){
+        if(typeof obj[i] === 'object'){
+            newObj[i] = deepCopy(obj[i],hash)
+        }else{
+            newObj[i] = obj[i] 
+        }
+    }
+    return newObj
+}
+
+let a = {
+    b:{
+        c:[1,2,3],
+        e:'wa'
+    }
+}
+copy(a)
+deepCopy(a)
