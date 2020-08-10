538c448de2e28f361cb545aacceb04cf67c2b2e5
王亚男 <wangyanan05@koolearn-inc.com>
2020-08-10 11:05:29 +0800

    feat:增加一道额外题目

--- js/每日练习.js
+++ js/每日练习.js
@@ -1,3 +1,5 @@
+// 练习题：https://juejin.im/post/6859121743869509646
+
 /* 2020年8月4日*/
 
 import { resolve } from "dns"
@@ -261,4 +263,47 @@ function Fun(name,sex) {
   
   const fun = myNew(Fun,'子君','男')
   console.log(fun.getUserInfo())
+
+
+  // 额外的一道题目
+  /* 
+    如下为一段代码，请完善sum函数，使得 sum(1,2,3,4,5,6) 函数返回值为 21 ,
+    需要在 sum 函数中调用 asyncAdd 函数，且不能修改asyncAdd函数
+  */
+
+  /**
+ * 请在 sum函数中调用此函数，完成数值计算
+ * @param {*} a 要相加的第一个值
+ * @param {*} b 要相加的第二个值
+ * @param {*} callback 相加之后的回调函数
+ */
+function asyncAdd(a,b,callback) {
+    setTimeout(function(){
+     callback(null, a+b)
+    },100)
+  }
+  
+  /**
+   * 请在此方法中调用asyncAdd方法，完成数值计算
+   * @param  {...any} rest 传入的参数
+   */
+  async function sum(...rest) {
+    // 请在此处完善代码
+    let result = rest.shift()
+    for(let num of rest){
+        result = await new Promise(resolve => {
+            asyncAdd(result,num,(_,res) => {
+                resolve(res)
+            })
+        })
+    }
+    return result
+  }
+  
+  let start = window.performance.now()
+  sum(1, 2, 3, 4, 5, 6).then(res => {
+    // 请保证在调用sum方法之后，返回结果21
+    console.log(res)
+    console.log(`程序执行共耗时: ${window.performance.now() - start}`)
+  })
   
