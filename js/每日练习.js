19317411beec9fdf24fb622f8cb110f0ded6666b
王亚男 <wangyanan05@koolearn-inc.com>
2020-08-14 10:28:03 +0800

    fix:实现虚拟dom转为真实dom

--- js/每日练习.js
+++ js/每日练习.js
@@ -330,4 +330,50 @@ function asyncAdd(a,b,callback) {
     console.log(res)
     console.log(`程序执行共耗时: ${window.performance.now() - start}`)
   })
+
+
+
+
+/* 2020年8月14日练习 */
+// Q: 实现一个简单的虚拟 DOM 渲染
+let domNode = {
+    tagName: 'ul',
+    props: { class: 'list' },
+    children: [{
+      tagName: 'li',
+      children: ['item1']
+    }, {
+      tagName: 'li',
+      children: ['item1']
+    }]
+  };
+  
+  // 构建一个 render 函数，将 domNode 对象渲染为 以下 dom
+  <ul class="list">
+      <li>item1</li>
+      <li>item2</li>
+  </ul>
+  
+
+function render(domNode){
+    if(!domNode) return document.createElement('')
+    let $el 
+    if(typeof domNode === 'object'){
+        $el = document.createElement(domNode.tagName)
+        if(domNode.props){
+            for(let i in domNode.props){
+                $el.setAttribute(i,domNode.props[i])
+            }   
+        }
+        if(domNode.children.length){
+            for(let i in domNode.children){
+                let temp = render(domNode.children[i])
+                $el.appendChild(temp)
+            }
+        }
+    }else{
+        $el = document.createTextNode(domNode)
+    }
+    return $el
+}
   
