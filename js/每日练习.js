fe26058b36de3de24aa445fd25ad50fa3f34231d
王亚男 <wangyanan05@koolearn-inc.com>
2020-09-07 10:42:04 +0800

    feat:9.7每日练习

--- js/每日练习.js
+++ js/每日练习.js
@@ -480,3 +480,45 @@ function compose(){
 let toUpperCase = (x) => x.toUpperCase()
 let exclaim = (x) => x + '!';
 compose(toUpperCase,exclaim)('hello world')
+
+
+/* 2020年9月7日练习 */
+let domNode = {
+    tagName: 'ul',
+    props: { class: 'list' },
+    children: [{
+      tagName: 'li',
+      children: ['item1']
+    }, {
+      tagName: 'li',
+      children: ['item1']
+    }]
+  };
+  
+  // 构建一个 render 函数，将 domNode 对象渲染为 以下 dom
+  <ul class="list">
+      <li>item1</li>
+      <li>item2</li>
+  </ul>
+
+  function render(domNode){
+    if(!domNode) return document.createDocumentFragment()
+    let dom 
+    if(typeof domNode === 'object'){
+        dom = document.createElement(domNode.tagName)
+        if(dom.props){
+            for(let i in dom.props){
+                dom.setAttribute(i,dom.props[i])
+            }
+        }
+        if(domNode.children.length){
+            for(let i in domNode.children){
+                let temp = render(domNode.children[i])
+                dom.appendChild(temp)
+            }
+        }
+    }else{
+        dom = document.createTextNode(domNode)
+    }
+    return dom
+  }
